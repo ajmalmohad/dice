772f376f67fa5d84595d5c82e201c2c6930c2365
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-12-24 21:04:59 +0530

    feat: eslint configuration

--- packages/dice-webapp/app/providers.tsx
+++ packages/dice-webapp/app/providers.tsx
@@ -3,28 +3,28 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import { NextUIProvider } from "@nextui-org/system";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
 export interface ProvidersProps {
-	children: React.ReactNode;
-	themeProps?: ThemeProviderProps;
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-  const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if(!mounted) return null
+  if (!mounted) return null;
 
-	return (
-		<NextUIProvider navigate={router.push}>
-			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-		</NextUIProvider>
-	);
+  return (
+    <NextUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </NextUIProvider>
+  );
 }
