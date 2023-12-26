9e0a52a9c4ab9fd544e1a51376d0e2fe6aa792f8
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-12-26 20:31:08 +0530

    feat: multitenant signin

--- packages/dice-webapp/components/auth/google-auth.tsx
+++ packages/dice-webapp/components/auth/google-auth.tsx
@@ -4,18 +4,25 @@ import { signIn } from "next-auth/react"
 import { signOut } from "next-auth/react"
 import { Button } from "@nextui-org/button"
 import { FaGoogle } from "react-icons/fa"
+import { useState } from "react"
+import { Switch } from "@nextui-org/switch"
 
 export function SignInWithGoogleButton() {
+  const [institution, setInstitution] = useState(false)
+
   return (
-    <Button onClick={() => {
-      signIn('google')
-    }}
-    size="lg"
-    className="dark:bg-white dark:text-black bg-black text-white rounded-md"
-    >
-      <p className="text-sm md:text-base font-bold">Login with Google</p>
-      <FaGoogle className="text-sm md:text-base" />
-    </Button>
+    <>
+      <Switch checked={institution} onChange={() => setInstitution(!institution)} />
+      <Button onClick={() => {
+        institution ? signIn('institution') : signIn('student')
+      }}
+      size="lg"
+      className="dark:bg-white dark:text-black bg-black text-white rounded-md"
+      >
+        <p className="text-sm md:text-base font-bold">Login with Google</p>
+        <FaGoogle className="text-sm md:text-base" />
+      </Button>
+    </>
   )
 }
 
