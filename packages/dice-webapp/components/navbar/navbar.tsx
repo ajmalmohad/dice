c11364da0e2a0f433e25eefab3e588cf54af728c
vishnuxprasad <vshnprsdvs+github@gmail.com>
2023-12-29 15:32:24 +0530

    feat: implemeted navbar

---
+++ packages/dice-webapp/components/navbar/navbar.tsx
@@ -0,0 +1,21 @@
+"use client"
+import React from "react";
+import { Input } from "@nextui-org/input";
+import {Avatar} from "@nextui-org/avatar";
+import { CiSearch } from "react-icons/ci";
+
+export default function Navbar() {
+  return (
+    <div className="w-full rounded-2xl flex items-center gap-4">
+      <Input
+        radius="lg"
+        variant="bordered"
+        placeholder="Type to search..."
+        startContent={
+          <CiSearch className="text-xl text-black/50 mb-0.5 dark:text-white/90 pointer-events-none" />
+        }
+      />
+      <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" size="lg" />
+    </div>
+  );
+}
