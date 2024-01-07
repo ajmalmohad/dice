ba762da90dc0dad20032e74c0d779bd7a9b5ca3b
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-01-07 19:32:13 +0530

    feat: hover and border customised

--- packages/dice-webapp/components/sidebar/sidebar.tsx
+++ packages/dice-webapp/components/sidebar/sidebar.tsx
@@ -11,29 +11,28 @@ import { IoMdLogOut } from "react-icons/io";
 import { SidebarItem } from "./sidebar-data";
 import Link from "next/link";
 
-export default function Sidebar({ sidebarData } : {
-  sidebarData: SidebarItem[]
+export default function Sidebar({
+  sidebarData,
+}: {
+  sidebarData: SidebarItem[];
 }) {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   function toggleSidebar() {
     setSidebarOpen(!isSidebarOpen);
   }
   return (
-    <div className="h-[100vh] w-fit max-w-[320px] flex flex-col border border-r-black">
+    <div className="h-[100vh] w-fit max-w-[320px] flex flex-col border-r-1 border-gray-400 dark:border-gray-800 ">
       <section className="flex items-center gap-4 text-3xl m-4 mb-8">
-        <IoMenuOutline onClick={toggleSidebar} className="text-black dark:text-white" />
+        <IoMenuOutline
+          onClick={toggleSidebar}
+          className="text-black dark:text-white"
+        />
         <div className={`flex items-center ${!isSidebarOpen ? "hidden" : ""}`}>
           <FaDiceD20 className="text-black dark:text-white" />
-          <Image
-            className="dark:invert ml-2"
-            src={LogoPath}
-            alt="DICE"
-          />
+          <Image className="dark:invert ml-2" src={LogoPath} alt="DICE" />
         </div>
       </section>
-      <main
-        className="h-full flex flex-col justify-between text-black dark:text-white"
-      >
+      <main className="h-full flex flex-col justify-between text-black dark:text-white">
         <div>
           {sidebarData.map((item, idx) => (
             <SidebarItem
@@ -47,7 +46,7 @@ export default function Sidebar({ sidebarData } : {
           ))}
         </div>
         <div
-          className="cursor-pointer p-4 text-3xl text-red-600 flex items-center gap-4 hover:bg-red-200"
+          className="cursor-pointer p-4 text-3xl text-red-600 flex items-center gap-4 hover:bg-red-600 hover:text-white hover:rounded-lg"
           onClick={() => {
             signOut();
           }}
@@ -67,13 +66,20 @@ function SidebarItem(props: SidebarItem & { isSidebarOpen: boolean }) {
   if (pathname === props.path) console.log(pathname);
 
   return (
-    <Link href={props.path ?? "#"}
-      className={`p-4 text-3xl flex items-center gap-4 border-black hover:bg-gray-200 ${props.path == pathname ? "border-r-4 border-black" : ""}}`}
+    <Link
+      href={props.path ?? "#"}
+      className={`p-4 text-3xl flex items-center gap-4 hover:bg-gray-300 hover:dark:bg-gray-900 ${
+        props.path == pathname
+          ? "border-r-3 border-black dark:border-white "
+          : ""
+      }}`}
     >
       <div>
         {props.path === pathname ? props.activeIcon : props.defaultIcon}
       </div>
-      <p className={`text-xl font-medium ${props.isSidebarOpen ? "" : "hidden"}`}>
+      <p
+        className={`text-xl font-medium ${props.isSidebarOpen ? "" : "hidden"}`}
+      >
         {props.title}
       </p>
     </Link>
