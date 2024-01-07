433aa842188d63bc57ba77b745564bbd031efc2e
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-01-07 18:02:06 +0530

    feat: added admin and user sidebaritems

--- packages/dice-webapp/components/sidebar/sidebar-data.tsx
+++ packages/dice-webapp/components/sidebar/sidebar-data.tsx
@@ -1,4 +1,6 @@
 import { LuLayoutDashboard } from "react-icons/lu";
+import { BsBuildingFillLock, BsBuildingGear } from "react-icons/bs";
+import { IoShare, IoShareOutline } from "react-icons/io5";
 import {
   BiSolidDashboard,
   BiMessageSquareError,
@@ -24,7 +26,34 @@ export type SidebarItem = {
   activeIcon: React.ReactNode;
 };
 
-export const orgSidebarData: SidebarItem[] = [
+export const AdmSidebarData: SidebarItem[] = [
+  {
+    title: "Dashboard",
+    path: "/",
+    defaultIcon: <LuLayoutDashboard />,
+    activeIcon: <BiSolidDashboard />,
+  },
+  {
+    title: "All Organizations",
+    path: "#",
+    defaultIcon: <BsBuildingGear />,
+    activeIcon: <BsBuildingFillLock />,
+  },
+  {
+    title: "Pending Requests",
+    path: "#",
+    defaultIcon: <PiClockCountdown />,
+    activeIcon: <PiClockCountdownFill />,
+  },
+  {
+    title: "My Profile",
+    path: "#",
+    defaultIcon: <PiUserBold />,
+    activeIcon: <PiUserFill />,
+  },
+];
+
+export const OrgSidebarData: SidebarItem[] = [
   {
     title: "Dashboard",
     path: "/",
@@ -54,6 +83,39 @@ export const orgSidebarData: SidebarItem[] = [
     path: "#",
     defaultIcon: <BiMessageSquareError />,
     activeIcon: <BiSolidMessageSquareError />,
+  },
+  {
+    title: "My Profile",
+    path: "#",
+    defaultIcon: <PiUserBold />,
+    activeIcon: <PiUserFill />,
+  },
+];
+
+export const StuSidebarData: SidebarItem[] = [
+  {
+    title: "Dashboard",
+    path: "/",
+    defaultIcon: <LuLayoutDashboard />,
+    activeIcon: <BiSolidDashboard />,
+  },
+  {
+    title: "My Credentials",
+    path: "#",
+    defaultIcon: <PiCertificate />,
+    activeIcon: <PiCertificateFill />,
+  },
+  {
+    title: "Pending Credentials",
+    path: "#",
+    defaultIcon: <PiClockCountdown />,
+    activeIcon: <PiClockCountdownFill />,
+  },
+  {
+    title: "Manage Shared",
+    path: "#",
+    defaultIcon: <IoShareOutline />,
+    activeIcon: <IoShare />,
   },
   {
     title: "My Profile",
