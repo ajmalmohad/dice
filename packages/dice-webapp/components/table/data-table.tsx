b53e65453b003a87ec826f74f2c4b063c74a3c22
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-01-21 22:55:46 +0530

    feat: added data table

--- packages/dice-webapp/components/table/data-table.tsx
+++ packages/dice-webapp/components/table/data-table.tsx
@@ -1,40 +1,40 @@
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/table/table"
 import getStudentCertificates from "../fetchers/getStudentCertificates"
 
 export function DataTable({ caption, content }: { caption: string, content: string }) {
-    let data: any = []
+  let data: any = []
 
-    if (content === 'student_cert') {
-        data = getStudentCertificates();
-    }
+  if (content === 'student_cert') {
+    data = getStudentCertificates();
+  }
 
-    return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Course</TableHead>
-                    <TableHead>Issuer</TableHead>
-                    <TableHead>IssueDate</TableHead>
-                    <TableHead className="text-right">Validity</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {data.map((item: any) => (
-                    <TableRow key={item.id}>
-                        <TableCell className="font-medium">{item.title}</TableCell>
-                        <TableCell>{item.issuer}</TableCell>
-                        <TableCell>{item.issueDate}</TableCell>
-                        <TableCell className="text-right">{item.expiryDate}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    )
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Course</TableHead>
+          <TableHead>Issuer</TableHead>
+          <TableHead>IssueDate</TableHead>
+          <TableHead className="text-right">Validity</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((item: any) => (
+          <TableRow key={item.id}>
+            <TableCell className="font-medium">{item.title}</TableCell>
+            <TableCell>{item.issuer}</TableCell>
+            <TableCell>{item.issueDate}</TableCell>
+            <TableCell className="text-right">{item.expiryDate}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
 }
