9ed614052e96a3e2dae58229c95cae396388714c
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-01-25 15:18:28 +0530

    refactor: fully removed next-ui

--- packages/dice-webapp/app/providers.tsx
+++ packages/dice-webapp/app/providers.tsx
@@ -2,8 +2,6 @@
 
 import * as React from "react";
 import { useState, useEffect } from "react";
-import { NextUIProvider } from "@nextui-org/system";
-import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { ClientSessionProvider } from "@/components/auth/client-session";
@@ -15,7 +13,6 @@ export interface ProvidersProps {
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   const [mounted, setMounted] = useState(false);
-  const router = useRouter();
 
   useEffect(() => {
     setMounted(true);
@@ -25,9 +22,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
   return (
     <ClientSessionProvider>
-      <NextUIProvider navigate={router.push}>
-        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-      </NextUIProvider>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
     </ClientSessionProvider>
   );
 }
