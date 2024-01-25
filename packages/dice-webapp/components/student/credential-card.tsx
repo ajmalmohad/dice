84a4fcc83f3d111f4cefaf0cda9a52c46a624bca
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-01-25 19:41:45 +0530

    feat: added credential card component

---
+++ packages/dice-webapp/components/student/credential-card.tsx
@@ -0,0 +1,49 @@
+import { Card, CardContent } from "../ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import { CiCircleChevRight } from "react-icons/ci";
+import { cn } from "@/lib/utils";
+
+type CredentialCardProps = {
+  imageLink: string;
+  title: string;
+  issuer: string;
+  issueDate: string;
+  className?: string;
+};
+
+export const CredentialCard = ({
+  imageLink,
+  title,
+  issuer,
+  issueDate,
+  className,
+}: CredentialCardProps) => {
+  return (
+    <Card className={cn("min-w-[200px]", className)}>
+      <CardContent className="flex p-6 justify-between items-center ">
+        <div className="flex">
+          <Avatar>
+            <AvatarImage src={imageLink} />
+            <AvatarFallback>#</AvatarFallback>
+          </Avatar>
+          <div>
+            <div className="flex">
+              <div className="ml-2 text-base font-medium">{title}</div>
+              <div className="ml-1 text-2xl">
+                <AiOutlineSafetyCertificate />
+              </div>
+            </div>
+            <div className="flex flex-col gap-1 text-xs text-ring font-normal sm:flex-row">
+              <div className="ml-2">issued by {issuer}</div>
+              <div className="ml-2 sm:ml-0">on {issueDate}</div>
+            </div>
+          </div>
+        </div>
+        <div className="ml-1 text-3xl">
+          <CiCircleChevRight />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
