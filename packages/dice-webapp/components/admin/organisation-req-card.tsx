30cf6cbce984c3f445975baef966e1308826523f
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-02-01 20:57:54 +0530

    refactor: code optimization

--- packages/dice-webapp/components/admin/organisation-req-card.tsx
+++ packages/dice-webapp/components/admin/organisation-req-card.tsx
@@ -2,8 +2,6 @@ import { Card, CardContent } from "../ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 
-import { cn } from "@/lib/utils";
-
 type OrganisationReqCardProps = {
   title: string;
   website: string;
@@ -18,8 +16,8 @@ export const OrganisationReqCard = ({
   className,
 }: OrganisationReqCardProps) => {
   return (
-    <Card className={cn("min-w-[350px]", className)}>
-      <CardContent className="flex flex-col items-center justify-between p-4 gap-6">
+    <Card className={className}>
+      <CardContent className="flex flex-col p-4 gap-6">
         <div className="flex gap-6 items-center">
           <Avatar>
             <AvatarImage src={imageLink} />
@@ -31,20 +29,17 @@ export const OrganisationReqCard = ({
           </div>
         </div>
         <div className="flex flex-col gap-3">
-          <div className="flex justify-between">
-            <Button
-              className="bg-green-500 text-white w-[110px] hover:bg-green-500/90 dark:bg-green-700  dark:hover:bg-green-700/90"
-              variant="destructive"
-            >
+          <div className="flex gap-3">
+            <Button className="grow bg-green-500 text-white w-full hover:bg-green-500/90 dark:bg-green-700  dark:hover:bg-green-700/90">
               Accept
             </Button>
-            <Button className="w-[110px]" variant="destructive">
+            <Button className="grow w-full" variant="destructive">
               Reject
             </Button>
           </div>
           <Button
-            className="bg-blue-500 text-white w-[250px]"
-            variant="outline"
+            className="bg-gray-500 text-white w-full hover:bg-gray-500/90 dark:bg-gray-700  dark:hover:bg-gray-700/90"
+            variant="secondary"
           >
             View
           </Button>
