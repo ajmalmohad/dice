1671cedd0ac9a5c65dbe124c720e7374eb029941
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-02-04 19:06:22 +0530

    feat: admin profile page added

---
+++ packages/dice-webapp/app/admin/profile/page.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/navbar/navbar";
+import { ProfileAccountTab } from "@/components/student/profile-account-tab";
+import { ProfileNameCard } from "@/components/student/profile-name-card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar className="mb-10" />
+      <ProfileNameCard
+        className="mb-10"
+        name="John Doe"
+        avatarUrl="https://i.pravatar.cc/150?u=a04258114e29026302d"
+      />
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+        </TabsList>
+        <TabsContent className="pt-4" value="account">
+          <ProfileAccountTab />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
