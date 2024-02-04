12b111cef933149fb54f9f7227fb868a07f4183e
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-02-04 19:15:22 +0530

    refactor: configured admin profile routing

--- packages/dice-webapp/components/sidebar/sidebar-data.tsx
+++ packages/dice-webapp/components/sidebar/sidebar-data.tsx
@@ -43,7 +43,7 @@ export const AdmSidebarData: SidebarItem[] = [
   },
   {
     title: "My Profile",
-    path: "#",
+    path: "/admin/profile",
     defaultIcon: <PiUserBold />,
     activeIcon: <PiUserFill />,
   },
