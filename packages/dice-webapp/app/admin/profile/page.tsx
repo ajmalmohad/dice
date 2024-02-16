9a7ccfa8f6512b48b71a2bd4efa3677d6b1b1c6d
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-02-16 20:57:25 +0530

    feat: fetching the user profile on server side

--- packages/dice-webapp/app/admin/profile/page.tsx
+++ packages/dice-webapp/app/admin/profile/page.tsx
@@ -2,22 +2,29 @@ import Navbar from "@/components/navbar/navbar";
 import { ProfileAccountTab } from "@/components/profile/profile-account-tab";
 import { ProfileNameCard } from "@/components/profile/profile-name-card";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { getUser } from "@/components/utils/get-server-user";
 
-export default function Page() {
+export default async function Page() {
+  const user = await getUser();
+  if (!user) {
+    return <div>Unauthorized</div>;
+  }
+
   return (
     <div>
       <Navbar className="mb-10" />
       <ProfileNameCard
         className="mb-10"
-        name="Bobbie Burns"
-        avatarUrl="https://i.pravatar.cc/150?u=f4cortoiuvoui"
-        role="Admin"
+        name={user.name}
+        avatarUrl={user.image || ""}
+        role={user.role}
       />
       <Tabs defaultValue="account" className="w-full">
         <TabsContent value="account">
           <ProfileAccountTab
-            name="Bobbie Burns"
-            profileUrl="https://i.pravatar.cc/150?u=f4cortoiuvoui"
+            name={user.name}
+            profileUrl={user.image || ""}
+            email={user.email}
           />
         </TabsContent>
       </Tabs>
--- packages/dice-webapp/components/profile/profile-account-tab.tsx
+++ packages/dice-webapp/components/profile/profile-account-tab.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
 export const ProfileAccountTab = ({
   name,
   profileUrl,
+  email,
 }: {
   name: string;
   profileUrl: string;
+  email: string;
 }) => {
   return (
     <div className="flex flex-col gap-10">
@@ -21,7 +25,14 @@ export const ProfileAccountTab = ({
               placeholder="Enter your name"
               defaultValue={name}
             />
-            <Button variant="secondary">Save</Button>
+            <Button
+              variant="secondary"
+              onClick={() => {
+                console.log("Save");
+              }}
+            >
+              Save
+            </Button>
           </div>
         </div>
         <div className="flex mt-4 flex-col gap-1 items-start max-w-[500px]">
@@ -32,7 +43,28 @@ export const ProfileAccountTab = ({
               placeholder="Enter profile url"
               defaultValue={profileUrl}
             />
-            <Button variant="secondary">Save</Button>
+            <Button
+              variant="secondary"
+              onClick={() => {
+                console.log("Save");
+              }}
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+        <div className="flex mt-4 flex-col gap-1 items-start max-w-[500px]">
+          <p className="text-xs text-ring">Profile Image</p>
+          <div className="flex gap-2 w-full">
+            <Input
+              className="grow"
+              placeholder="Email"
+              defaultValue={email}
+              disabled
+            />
+            <Button variant="secondary" disabled>
+              Save
+            </Button>
           </div>
         </div>
       </div>
