88a19470d09d7513ee9ada5c4964002518ea9fce
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-02-23 16:24:31 +0530

    chore: better db seeding

--- packages/dice-webapp/prisma/seed.ts
+++ packages/dice-webapp/prisma/seed.ts
@@ -3,72 +3,58 @@ import { hash } from "bcrypt";
 
 const prisma = new PrismaClient();
 
+async function addUser(name: string, email: string, password:string, role: string) {
+  password = await hash(password, 12);
+  return await prisma.user.upsert({
+    where: { email },
+    update: {},
+    create: {
+      name,
+      email,
+      password,
+      role,
+    },
+  });
+}
+
+async function addStudent(name: string, email: string) {
+  return await addUser(name, email, email, "STUDENT");
+}
+
+async function addInstitution(name: string, email: string) {
+  return await addUser(name, email, email, "INSTITUTION");
+}
+
+async function addPendingInstitution(name: string, email: string) {
+  return await addUser(name, email, email, "PENDING_INSTITUTION");
+}
+
+async function addAdmin(name: string, email: string) {
+  return await addUser(name, email, email, "ADMIN");
+}
+
+async function addWallet(userId: string, walletID: string) {
+  await prisma.wallets.upsert({
+    where: { walletID },
+    update: {},
+    create: {
+      walletID,
+      userId,
+    },
+  });
+}
+
 async function main() {
-  let password = await hash("john@doe.com", 12);
-  let user = await prisma.user.upsert({
-    where: { email: "john@doe.com" },
-    update: {},
-    create: {
-      name: "John Doe",
-      email: "john@doe.com",
-      password,
-      role: "STUDENT",
-    },
-  });
+  let student1 = await addStudent("John Doe", "john@doe.com");
+  await addWallet(student1.id, "0x1233489809238948932");
+  await addWallet(student1.id, "0x8980923812334948933");
 
-  await prisma.wallets.upsert({
-    where: { walletID: "0x1233489809238948932" },
-    update: {},
-    create: {
-      walletID: "0x1233489809238948932",
-      userId: user.id,
-    },
-  });
-
-  await prisma.wallets.upsert({
-    where: { walletID: "0x32432423238948932" },
-    update: {},
-    create: {
-      walletID: "0x32432423238948932",
-      userId: user.id,
-    },
-  });
-
-  password = await hash("admin@ktu.com", 12);
-  await prisma.user.upsert({
-    where: { email: "admin@ktu.com" },
-    update: {},
-    create: {
-      name: "Kalam Technical University",
-      email: "admin@ktu.com",
-      password,
-      role: "INSTITUTION",
-    },
-  });
-
-  password = await hash("admin@cusat.com", 12);
-  await prisma.user.upsert({
-    where: { email: "admin@cusat.com" },
-    update: {},
-    create: {
-      name: "Cochin University",
-      email: "admin@cusat.com",
-      password,
-      role: "PENDING_INSTITUTION",
-    },
-  });
-
-  password = await hash("admin@dice.com", 12);
-  await prisma.user.upsert({
-    where: { email: "admin@dice.com" },
-    update: {},
-    create: {
-      name: "DICE Admin",
-      email: "admin@dice.com",
-      password,
-      role: "ADMIN",
-    },
-  });
+  let institution1 = await addInstitution("Kalam Technical University", "admin@ktu.com");
+  await addWallet(institution1.id, "0x2389482334189809932");
+  
+  await addInstitution("Kerala University", "admin@kerala.com");
+  await addPendingInstitution("Cochin University", "admin@cusat.com");
+  await addAdmin("Admin", "admin@dice.com");
 }
 
 main()
