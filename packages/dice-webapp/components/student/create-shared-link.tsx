880ed22616c5ac5449064304bdea261657087c91
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-03-08 18:36:36 +0530

    feat: added create shared link dialog

---
+++ packages/dice-webapp/components/student/create-shared-link.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useToast } from "../ui/use-toast";
+import { Toaster } from "../ui/toaster";
+
+export function CreateSharedLink() {
+  const { toast } = useToast();
+
+  const addSharedLink = () => {
+    toast({
+      title: "Link Added",
+      description: "The shared link has been added",
+    });
+  };
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Add New</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add Shared Link</DialogTitle>
+            <DialogDescription>
+              Create a shared link with your credentials to share to the public
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Input
+                id="linkname"
+                placeholder="Link Name"
+                className="col-span-4"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Input
+                id="selectedcreds"
+                placeholder="Select Credentials"
+                className="col-span-4"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button onClick={addSharedLink}>Add Link</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Toaster />
+    </div>
+  );
+}
