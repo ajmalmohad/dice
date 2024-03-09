a1c31774e18b15b54261e8ad25b5a7a17faef2ed
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-03-09 14:25:57 +0530

    feat: added contract to the context

--- packages/dice-webapp/components/auth/web3-provider.tsx
+++ packages/dice-webapp/components/auth/web3-provider.tsx
@@ -10,6 +10,7 @@ declare global {
 interface IWeb3Context {
   web3: Web3 | null;
   address: string | null;
+  contract: any | null;
   error: string | null;
 }
 
@@ -18,9 +19,13 @@ export const Web3Context = createContext<IWeb3Context | null>(null);
 const Web3Provider = ({ children }: { children: ReactNode }) => {
   const [web3, setWeb3] = useState<Web3 | null>(null);
   const [address, setAddress] = useState<string | null>(null);
+  const [contract, setContract] = useState<any | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
+    const abi: any = []; // YOUR_CONTRACT_ABI
+    const contractAddress = "YOUR_CONTRACT_ADDRESS";
+
     if (window.ethereum) {
       window.ethereum
         .request({ method: "eth_requestAccounts" })
@@ -28,6 +33,12 @@ const Web3Provider = ({ children }: { children: ReactNode }) => {
           setAddress(accounts[0]);
           let w3 = new Web3(window.ethereum);
           setWeb3(w3);
+          try {
+            let contr = new w3.eth.Contract(abi, contractAddress);
+            setContract(contr);
+          } catch (err) {
+            setError("Failed to connect to contract");
+          }
         })
         .catch((err: Error) => setError(err.message));
 
@@ -38,6 +49,7 @@ const Web3Provider = ({ children }: { children: ReactNode }) => {
       window.ethereum.on("disconnect", () => {
         setAddress(null);
         setWeb3(null);
+        setContract(null);
       });
     } else {
       setError("Please install MetaMask");
@@ -45,7 +57,7 @@ const Web3Provider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   return (
-    <Web3Context.Provider value={{ web3, address, error }}>
+    <Web3Context.Provider value={{ web3, address, contract, error }}>
       {children}
     </Web3Context.Provider>
   );
