1a89b4b6c9b190ec4e7f58e5309dc940d6f280d6
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-03-09 15:49:57 +0530

    feat: navbar image fetch and update

--- packages/dice-webapp/components/navbar/navbar.tsx
+++ packages/dice-webapp/components/navbar/navbar.tsx
@@ -1,19 +1,34 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { Input } from "../ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 import { cn } from "@/lib/utils";
+import { getInitials } from "../utils/formatter";
+import { useSession } from "next-auth/react";
 
 export default function Navbar({ className }: { className?: string }) {
+  const { data: session } = useSession();
+  let [image, setImage] = React.useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      let res = await fetch("/api/profile/image");
+      let data = await res.json();
+      setImage(data.image);
+    })();
+  }, []);
+
   return (
     <div
       className={cn("w-full rounded-2xl flex items-center gap-4", className)}
     >
       <Input className="p-6" placeholder="Type to search..." />
       <Avatar>
-        <AvatarImage src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
-        <AvatarFallback>AM</AvatarFallback>
+        <AvatarImage src={image} />
+        <AvatarFallback>
+          {getInitials(session?.user?.name ? session.user.name : "USER")}
+        </AvatarFallback>
       </Avatar>
     </div>
   );
