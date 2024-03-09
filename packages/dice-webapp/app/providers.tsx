a9a2c231a6aa91c45ad90e50729f8d71eb9c058c
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-03-09 14:10:13 +0530

    feat: web3.js provider setup

--- packages/dice-webapp/app/providers.tsx
+++ packages/dice-webapp/app/providers.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { ClientSessionProvider } from "@/components/auth/client-session";
+import { Web3Provider } from "@/components/auth/web3-provider";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -21,8 +22,10 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   if (!mounted) return null;
 
   return (
-    <ClientSessionProvider>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </ClientSessionProvider>
+    <Web3Provider>
+      <ClientSessionProvider>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </ClientSessionProvider>
+    </Web3Provider>
   );
 }
