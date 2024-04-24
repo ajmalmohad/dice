9cd6ffe7285b5ecfdb81440cc35ffb9823742fcd
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-04-24 10:12:08 +0530

    chore: scripts add

--- packages/dice-webapp/prisma/seed.ts
+++ packages/dice-webapp/prisma/seed.ts
@@ -13,26 +13,6 @@ type User = {
   image: string | null;
 };
 
-type SharedLink = {
-  id: string;
-  linkName: string | null;
-  active: boolean;
-  userId: string;
-};
-
-type StudentCredential = {
-  id: string;
-  credentialType: string;
-  credentialLink: string;
-  issuerWallet: string;
-  transactionId: string;
-  issueDate: Date;
-  verified: boolean | null;
-  pending: boolean;
-  userId: string;
-  issuerId: string;
-};
-
 async function addUser(
   name: string,
   email: string,
@@ -52,78 +32,8 @@ async function addUser(
   });
 }
 
-async function addCredential(
-  type: string,
-  issuer: User,
-  reciever: User,
-  pending: boolean = false,
-): Promise<StudentCredential> {
-  return await prisma.studentCredentials.create({
-    data: {
-      credentialId: parseInt(randomBytes(2).toString("hex"), 16),
-      credentialType: type,
-      credentialLink: "https://example.com/credential.pdf",
-      issuerWallet: "0x" + randomBytes(20).toString("hex"),
-      transactionId: crypto.randomUUID(),
-      userId: reciever.id,
-      issuerId: issuer.id,
-      verified: true,
-      pending: pending,
-    },
-  });
-}
-
-async function addRejectedCredential(
-  type: string,
-  issuer: User,
-  reciever: User,
-) {
-  return await prisma.rejectedCredentials.create({
-    data: {
-      credentialType: type,
-      credentialLink: "https://example.com/rejected_credential.pdf",
-      issuerWallet: "0x" + randomBytes(20).toString("hex"),
-      transactionId: crypto.randomUUID(),
-      userId: reciever.id,
-      issuerId: issuer.id,
-      verified: false,
-    },
-  });
-}
-
-async function createSharedLink(
-  userId: string,
-  linkName: string,
-): Promise<SharedLink> {
-  const sharedLink = await prisma.sharedLink.create({
-    data: {
-      linkName,
-      active: true,
-      userId,
-    },
-  });
-
-  return sharedLink;
-}
-
-async function addLinkEntry(
-  sharedLink: SharedLink,
-  studentCredential: StudentCredential,
-) {
-  await prisma.linkEntry.create({
-    data: {
-      sharedLinkId: sharedLink.id,
-      studentCredentialId: studentCredential.id,
-    },
-  });
-}
-
 async function addStudent(name: string, email: string): Promise<User> {
   return await addUser(name, email, email, "STUDENT");
-}
-
-async function addInstitution(name: string, email: string): Promise<User> {
-  return await addUser(name, email, email, "INSTITUTION");
 }
 
 async function addPendingInstitution(
@@ -167,57 +77,26 @@ async function addWallet(institution: User, walletId: string) {
 async function main() {
   let student1 = await addStudent("John Doe", "john@doe.com");
 
-  let institution1 = await addInstitution(
+  await addWallet(student1, "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
+
+  let institution1 = await addPendingInstitution(
     "Kalam Technical University",
     "admin@ktu.com",
   );
 
-  let institution2 = await addInstitution(
-    "Kerala University",
-    "admin@kerala.com",
-  );
-
-  let credential1 = await addCredential("B.Tech", institution1, student1);
-  let credential2 = await addCredential("BSc.", institution2, student1);
-  await addCredential("M.Tech", institution1, student1, true);
-  await addCredential("MSc.", institution2, student1, true);
-  await addCredential("NPTEL", institution1, student1, true);
-  await addCredential("Internship", institution2, student1, true);
-  await addRejectedCredential("B.Tech", institution1, student1);
-  await addRejectedCredential("B.Sc", institution2, student1);
-
-  let sharedLink1 = await createSharedLink(
-    student1.id,
-    "Undergrad Credentials",
-  );
-  let sharedLink2 = await createSharedLink(
-    student1.id,
-    "Engineering Credentials",
-  );
-  let sharedLink3 = await createSharedLink(student1.id, "Degree Credentials");
-
-  await addLinkEntry(sharedLink1, credential1);
-  await addLinkEntry(sharedLink1, credential2);
-
-  await addLinkEntry(sharedLink2, credential1);
-
-  await addLinkEntry(sharedLink3, credential2);
-
-  let institution3 = await addPendingInstitution(
+  await addPendingInstitution(
     "Cochin University",
     "admin@cusat.com",
   );
 
-  await addWallet(institution3, "0x" + randomBytes(20).toString("hex"));
-
   await addApplicationForm(
-    institution3,
-    "Kochi Kerala",
-    "JJSJFDSAUFS45345",
+    institution1,
+    "Trivandrum Kerala",
+    "JSHAFKSHKFRUFFNFW",
     "45898345934",
   );
 
-  await addPendingInstitution("Anna University", "admin@anna.com");
+  await addWallet(institution1, "0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
 
   await addAdmin("Admin", "admin@dice.com");
 }
